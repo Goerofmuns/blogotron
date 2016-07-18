12f8e5e2cec730af0b0a1453cd57a81560a9e935
Tom Hutchings <goerofmuns@gmail.com>
2016-07-18 16:12:37 -0700

    pppooooorrrtttsss

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT() || 8080;
+
 app.get('/', function(res, req) {
     res.send("<h1>Hello World</h1>");
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
 
 });

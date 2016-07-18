6aaa4bead8d56effcdff5d7bb144e0fbf8bccf0b
Tom Hutchings <goerofmuns@gmail.com>
2016-07-18 16:14:03 -0700

    its a loud airport okay?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT() || 8080;
+var port = process.env.PORT || 8080;
 
 app.get('/', function(res, req) {
     res.send("<h1>Hello World</h1>");
